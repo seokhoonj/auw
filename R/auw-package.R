6ffd5a9368d5997c905bddbc260cc47b82184883
seokhoonj <seokhoonj@gmail.com>
2024-03-05 17:32:50 +0900

    `jaid::paste_list()` function is added for NAMESPACE

--- R/auw-package.R
+++ R/auw-package.R
@@ -5,7 +5,8 @@
 #' @import ggshort
 #' @importFrom data.table `:=` `.SD` copy set setattr setcolorder setDT setnames
 #' setorder setorderv
-#' @importFrom jaid add_mon replace_na_with_zero rm_cols rotate set_dt unilen
+#' @importFrom jaid add_mon paste_list replace_na_with_zero rm_cols rotate set_dt
+#' unilen
 #' @importFrom rlang as_name enquo enquos ensym ensyms list2 syms
 #' @importFrom scales comma
 #' @importFrom stats fisher.test formula
