5179d1016a892317e0ad0d50f54bc3d123db4fde
seokhoonj <seokhoonj@gmail.com>
2024-03-25 13:43:39 +0900

    `get_stat_alr()` variables is modified

--- R/actual.R
+++ R/actual.R
@@ -6,38 +6,38 @@
 #' @param group_var name of columns
 #' @param value_var name of columns specifying loss, rp
 #' @param period_var a name of column specifying period
-#' @param elapse_var a name of column specifying elapsed period
+#' @param elapsed_var a name of column specifying elapsed period
 #' @return a data.frame
 #'
 #' @export
 get_stat_alr <- function(df, group_var, value_var = c("loss", "rp"),
-                         period_var = c("uym"), elapse_var = c("elp")) {
+                         period_var = c("uym"), elapsed_var = c("elpm")) {
   jaid::has_ptr(df, error_raise = TRUE)
-  group_var  <- match_cols(df, sapply(rlang::enexpr(group_var), rlang::as_name))
-  value_var  <- match_cols(df, sapply(rlang::enexpr(value_var), rlang::as_name))
-  period_var <- match_cols(df, sapply(rlang::enexpr(period_var), rlang::as_name))
-  elapse_var <- match_cols(df, sapply(rlang::enexpr(elapse_var), rlang::as_name))
-  group_period_var <- c(group_var, period_var)
-  group_elapse_var <- c(group_var, elapse_var)
-  group_period_elapse_var <- c(group_var, period_var, elapse_var)
+  grp_var <- match_cols(df, sapply(rlang::enexpr(group_var), rlang::as_name))
+  val_var <- match_cols(df, sapply(rlang::enexpr(value_var), rlang::as_name))
+  prd_var <- match_cols(df, sapply(rlang::enexpr(period_var), rlang::as_name))
+  elp_var <- match_cols(df, sapply(rlang::enexpr(elapsed_var), rlang::as_name))
+  grp_prd_var <- c(grp_var, prd_var)
+  grp_elp_var <- c(grp_var, elp_var)
+  grp_prd_elp_var <- c(grp_var, prd_var, elp_var)
   old_class <- class(df)
   jaid::set_dt(df)
   closs <- clr <- cmargin <- crp <- loss <- lr <- n_sample <- rp <- NULL
-  dn <- jaid::get_stat_by(df, group_var = !!group_elapse_var,
-                          value_var = !!period_var, fun = jaid::unilen)
-  data.table::setnames(dn, period_var, "n_sample")
-  dt <- jaid::get_stat_by(df, group_var = !!group_period_elapse_var,
-                          value_var = !!value_var)
-  dt[dn, on = group_elapse_var, `:=`(n_sample, n_sample)]
-  data.table::setcolorder(dt, "n_sample", before = period_var)
-  jaid::set_stat_by(dt, group_var = !!group_period_var,
-                    value_var = !!value_var, fun = cumsum)
+  dn <- jaid::get_stat_by(df, group_var = !!grp_elp_var,
+                          value_var = !!prd_var, fun = jaid::unilen)
+  data.table::setnames(dn, prd_var, "n_sample")
+  dt <- jaid::get_stat_by(df, group_var = !!grp_prd_elp_var,
+                          value_var = !!val_var)
+  dt[dn, on = grp_prd_var, `:=`(n_sample, n_sample)]
+  data.table::setcolorder(dt, "n_sample", before = prd_var)
+  jaid::set_stat_by(dt, group_var = !!grp_prd_var,
+                    value_var = !!val_var, fun = cumsum)
   dt[, `:=`(margin, rp - loss)]
   dt[, `:=`(cmargin, crp - closs)]
   dt[, `:=`(lr, loss / rp)]
   dt[, `:=`(clr, closs / crp)]
 
-  dc <- data.table::melt(dt, id.vars = group_period_elapse_var,
+  dc <- data.table::melt(dt, id.vars = grp_prd_elp_var,
                          measure.vars = c("closs", "cmargin", "crp"))
   dm <- dt[, .(
     n_sample   = .N,
@@ -49,7 +49,7 @@ get_stat_alr <- function(df, group_var, value_var = c("loss", "rp"),
     lr_se_upp  = mean(lr)  + mean(lr)  / sqrt(.N),
     clr_se_lwr = mean(clr) - mean(clr) / sqrt(.N),
     clr_se_upp = mean(clr) + mean(clr) / sqrt(.N)
-  ), keyby = group_elapse_var]
+  ), keyby = grp_elp_var]
 
   jaid::set_attr(dc, "class", c("cumsum", old_class))
   jaid::set_attr(dm, "class", c("mean", old_class))
