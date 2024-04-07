7a744fe8a107dbd854e1c977f872fcad677fa1b3
seokhoonj <seokhoonj@gmail.com>
2024-04-07 15:37:22 +0900

    `ggshort_theme()` is removed

--- R/theme.R
+++
@@ -1,8 +0,0 @@
-
-ggshort_theme <- function(theme = c("view", "save", "shiny"), ...) {
-  theme <- match.arg(theme)
-  switch(theme,
-         view  = ggshort::theme_view(...),
-         save  = ggshort::theme_save(...),
-         shiny = ggshort::theme_shiny(...))
-}
